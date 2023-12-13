e508ee01ff9f3963382faec432c5e68e705ebe00
HyShin <hyashin1998@gmail.com>
2023-12-14 02:06:32 +0900

    Functionaization/ title generation
    Make diary generating feature to function.
    Add Title on diary.

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ from sqlalchemy import create_engine, Column, Integer, String, Text, Boolean, Da
 from sqlalchemy.orm import sessionmaker,Session, relationship
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
+from typing import Tuple
 import requests
 import base64
 from dotenv import load_dotenv
 import openai
 import os
+import deepl
 
 # 환경 변수 로드
 load_dotenv()
@@ -25,6 +27,7 @@ API_HOST = os.getenv('API_HOST', 'https://api.stability.ai')
 S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+DEEPL_API_KEY = os.getenv("DEEPL_API_KEY")
 
 # 데이터베이스 연결 설정
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
@@ -47,6 +50,9 @@ app = FastAPI()
 
 # OpenAI GPT-3 설정
 openai.api_key = OPENAI_API_KEY
+
+# DeepL 번역 객체 생성
+translator = deepl.Translator(DEEPL_API_KEY)
 
 # 데이터베이스 세션 의존성 주입을 위한 함수 추가
 def get_db():
@@ -79,15 +85,49 @@ def upload_file_to_s3(file_content, file_name, bucket_name, aws_access_key_id, a
         return f"https://{bucket_name}.s3.amazonaws.com/{file_name}"
     except NoCredentialsError:
         raise Exception("AWS credentials are not available")
+
+
+# location, people, experience 번역 함수
+def translate_fields( location, people, experience):
+    translated_location = translator.translate_text(location, target_lang="EN-US").text
+    translated_people = translator.translate_text(people, target_lang="EN-US").text
+    translated_experience = translator.translate_text(experience, target_lang="EN-US").text
+    return translated_location, translated_people, translated_experience
+
+#생성된 영어를 한국어로 번역하는 함수
+def translate_to_korean(text: str) -> str:
+    translated_text = translator.translate_text(text, target_lang="KO").text
+    return translated_text
     
 
-# 그림 생성을 위한 요약 생성 함수
+# 제목 생성 함수
 def generate_summary(date, location, people, experience):
     summary_instruct = [
         {
             "role": "system",
             "content": "Please generate a sentence using the given parameters that can be used as a title for a diary entry. "
-                       + "In 50 letters or less than 50 letters "
+                       + "In 20 or more and 60 or less characters"
+                       +f"Date: {date}, Location: {location}, People: {people}, Experience: {experience}"
+        }
+    ]
+
+    response = openai.ChatCompletion.create(
+        model="ft:gpt-3.5-turbo-1106:personal::8RpLwK9R",
+        messages=summary_instruct,
+        max_tokens=100,
+        temperature=0.5,
+        top_p=1
+    )
+    summary = response.choices[0].message.content.strip()
+    return summary
+
+#  Stable Diffusion 입력용 요약함수
+def generate_summary2(date, location, people, experience):
+    summary_instruct = [
+        {
+            "role": "system",
+            "content": "Please generate a sentence using the given parameters that can be used as a title for a diary entry. "
+                       + "In 20 or more and 1500 or less characters"
                        +f"Date: {date}, Location: {location}, People: {people}, Experience: {experience}"
         }
     ]
@@ -103,29 +143,25 @@ def generate_summary(date, location, people, experience):
     return summary
 
 
-# 일기 생성 API 엔드포인트
-@app.post("/diary/create")
-async def create_diary(request: DiaryRequest, db: Session = Depends(get_db)):
+#일기(텍스트) 생성함수
+def generate_diary(date, location, people, happy, comfortable, sad, angry, experience):
     messages = [
         {
             "role": "system",
             "content": "You are a program that receives keywords related to time, location, people, and experience to generate diaries. "
                        + "The main information provided will include emotional state scores (comfortable, happy, angry, sad), date, location, and people as basic information, "
                        + "and the user will input text about their experiences that day.\n\nBased on the information provided, please write a diary. "
-                       + "The diary will be written in Korean."
                        + "make the content longer and more natural"
                        + "Don't talk about emotion's score"
         },
         {
             "role": "user",
-            "content": (f"Date: {request.date}, Location: {request.location}, People: {request.people}, "
-                        f"Happy: {request.happy}, Comfortable: {request.comfortable}, Sad: {request.sad}, "
-                        f"Angry: {request.angry}, Experience: {request.experience}")
+            "content": (f"Date: {date}, Location: {location}, People: {people}, "
+                        f"Happy: {happy}, Comfortable: {comfortable}, Sad: {sad}, "
+                        f"Angry: {angry}, Experience: {experience}")
         }
     ]
-    
-    try:
-        response = openai.ChatCompletion.create(
+    response = openai.ChatCompletion.create(
             model="ft:gpt-3.5-turbo-1106:personal::8RgY8JYa",
             messages = messages,
             temperature=1,
@@ -134,10 +170,30 @@ async def create_diary(request: DiaryRequest, db: Session = Depends(get_db)):
             frequency_penalty=0.7,
             presence_penalty=0
         )
-        content = response.choices[0].message.content.strip()
+    content = response.choices[0].message.content.strip()
+    return content
+
+
+
+# 일기 생성 API 엔드포인트
+@app.post("/diary/create")
+async def create_diary(request: DiaryRequest, db: Session = Depends(get_db)):
+    
+    try:
+        #한글 요청값 영어로 변경
+        location_EN, people_EN, experience_EN = translate_fields(request.location, request.people, request.experience)
+        print(f"Location (EN): {location_EN}")
+        print(f"People (EN): {people_EN}")
+        print(f"Experience (EN): {experience_EN}")
+       
+        #일기 생성
+        content = generate_diary(request.date, location_EN, people_EN, request.happy, request.comfortable, request.sad, request.angry, experience_EN)
+        content_kr = translate_to_korean(content)
         
-        # 요약 생성
-        summary_content = generate_summary(request.date, request.location, request.people, request.experience)
+        #제목 생성
+        summary_content = generate_summary(request.date, location_EN, people_EN, experience_EN)
+        summary_sd = generate_summary2(request.date, location_EN, people_EN, experience_EN)
+        title_kr = translate_to_korean(summary_content)
 
         # Stable Diffusion API 호출 및 이미지 생성
         response = requests.post(
@@ -150,7 +206,7 @@ async def create_diary(request: DiaryRequest, db: Session = Depends(get_db)):
             json={
                 "text_prompts":
                 [{
-                    "text": "With No people," + summary_content, 
+                    "text": "With No people," + summary_sd, 
                     "weight": 1
                 },
                 {
@@ -188,7 +244,8 @@ async def create_diary(request: DiaryRequest, db: Session = Depends(get_db)):
 
         # Diary 인스턴스 생성 및 저장
         new_diary = Diary()
-        new_diary.content = content # GPT-3 응답으로부터 생성된 내용
+        new_diary.content = content_kr # GPT-3 응답으로부터 생성된 내용
+        new_diary.title = title_kr
         new_diary.member_id = request.member_id
         new_diary.img = image_url
         new_diary.is_liked =False
@@ -234,43 +291,20 @@ async def recreate_diary(diary_id: int, db: Session = Depends(get_db)):
         if not diary_info:
             raise HTTPException(status_code=404, detail=f"DiaryInfo for diary id {diary_id} not found")
 
+        #입력값 번역
+        place_EN, person_EN, experience_EN = translate_fields(diary_info.place, diary_info.person, diary_info.experience)
         #  diary_info의 값을 이용해 GPT-3 API 호출
-        gpt_messages = [
-            {
-                "role": "system",
-                "content": "You are a program that receives keywords related to time, location, people, and experience to generate diaries. "
-                       + "The main information provided will include emotional state scores (comfortable, happy, angry, sad), date, location, and people as basic information, "
-                       + "and the user will input text about their experiences that day.\n\nBased on the information provided, please write a diary. "
-                       + "The diary will be written in Korean."
-                       + "make the content longer and more natural"
-                       + "Don't talk about emotion's score"
-        
-            },
-            {
-                "role": "user",
-                "content": (f"Date: {diary_info.date}, Location: {diary_info.place}, People: {diary_info.person}, "
-                            f"Happy: {diary_info.happy}, Comfortable: {diary_info.comfortable}, Sad: {diary_info.sad}, "
-                            f"Angry: {diary_info.angry}, Experience: {diary_info.experience}")
-            }
-        ]
+        new_content = generate_diary(diary_info.date, place_EN, person_EN, diary_info.happy, diary_info.comfortable, diary_info.sad, diary_info.angry, experience_EN)
+        content_kr = translate_to_korean(new_content)
 
-        gpt_response = openai.ChatCompletion.create(
-            model="ft:gpt-3.5-turbo-1106:personal::8RgY8JYa",
-            messages=gpt_messages,
-            temperature=1,
-            max_tokens=1000,
-            top_p=1,
-            frequency_penalty=0.7,
-            presence_penalty=0
-        )
-        new_content = gpt_response.choices[0].message.content.strip()
+
 
         # Diary 내용 업데이트
         diary = db.query(Diary).filter(Diary.id == diary_id).first()
         if not diary:
             raise HTTPException(status_code=404, detail=f"Diary with id {diary_id} not found")
 
-        diary.content = new_content
+        diary.content = content_kr
         diary.modified_at = datetime.now()
         db.commit()
 
@@ -297,7 +331,7 @@ async def regenerate_diary_image(diary_id: int, db: Session = Depends(get_db)):
             raise HTTPException(status_code=404, detail=f"DiaryInfo for diary id {diary_id} not found")
         
         # 요약 생성
-        summary_content = generate_summary(diary_info.date, diary_info.place, diary_info.person, diary_info.experience)
+        summary_sd = generate_summary2(diary_info.date, diary_info.place, diary_info.person, diary_info.experience)
 
 
         # Stable Diffusion API 호출 및 이미지 생성
@@ -311,7 +345,7 @@ async def regenerate_diary_image(diary_id: int, db: Session = Depends(get_db)):
             json={
                 "text_prompts":
                 [{
-                    "text": "With No people," + summary_content, 
+                    "text": "With No people," + summary_sd, 
                     "weight": 1
                 },
                 {
